fcad9cc2c9d944aa06aa71fb7d7b04c0d9a7b975
jakeibeemassa <jac.massa0908@gmail.com>
2015-03-10 00:32:27 -0500

    Add drawer menu

---
+++ app/src/main/java/com/infiniteloop/mytasks/DrawerItem.java
@@ -0,0 +1,42 @@
+package com.infiniteloop.mytasks;
+
+/**
+ * Created by theotherside on 09/03/15.
+ */
+public class DrawerItem {
+
+    private String mTitle;
+    private int mImage;
+    private int mType;
+
+    public DrawerItem(String title, int image, int type){
+        mImage=image;
+        mTitle=title;
+        mType= type;
+    }
+
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public int getImage() {
+        return mImage;
+    }
+
+    public void setImage(int mImage) {
+        this.mImage = mImage;
+    }
+
+    public int getType() {
+        return mType;
+    }
+
+    public void setType(int mType) {
+        this.mType = mType;
+    }
+}
