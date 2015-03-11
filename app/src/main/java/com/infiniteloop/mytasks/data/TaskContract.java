1259c58d08379c2c7eb2e35bdcbcd53f5309c90e
jakeibeemassa <jac.massa0908@gmail.com>
2015-03-11 13:05:08 -0500

    Add Database contract

---
+++ app/src/main/java/com/infiniteloop/mytasks/data/TaskContract.java
@@ -0,0 +1,42 @@
+package com.infiniteloop.mytasks.data;
+
+import android.provider.BaseColumns;
+import android.text.format.Time;
+
+/**
+ * Created by theotherside on 11/03/15.
+ * Defines tables and column names for the task database
+ */
+
+public class TaskContract {
+
+    //To make it easy to query for the exact date , we normalize all dates that go into
+    // the database to the start of the Juilian day at UTC
+
+    public static long normalizeDate(long date){
+        //Normalize the date to the begining of the (UTC) day
+        Time time = new Time();
+        time.set(date);
+        int julianDay= Time.getJulianDay(date,time.gmtoff);
+        return time.setJulianDay(julianDay);
+    }
+
+    public static final class CategoryEntry implements BaseColumns{
+
+        public static final String TABLE_NAME="category";
+
+        public static final String COLUMN_CATEGORY_ID="category_id";
+        public static final String COLUMN_NAME="name";
+    }
+
+    public static final class TaskEntry implements BaseColumns{
+        public static final String TABLE_NAME="task";
+
+        public static final String COLUMN_CAT_KEY="category_id";
+        public static final String COLUMN_DATE="date";
+        public static final String COLUMN_TASK_ID="task_id";
+        public static final String COLUMN_TASK_TITLE="title";
+        public static final String COLUMN_PRIORITY="priority";
+        public static final String COLUMN_DURATION="duration";
+    }
+}
---
+++ app/src/main/java/com/infiniteloop/mytasks/data/TaskDataBaseHelper.java
@@ -0,0 +1,7 @@
+package com.infiniteloop.mytasks.data;
+
+/**
+ * Created by theotherside on 11/03/15.
+ */
+public class TaskDataBaseHelper {
+}
