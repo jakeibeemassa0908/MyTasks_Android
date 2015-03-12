1ec1cc5c4fc7493d8a9508e1d3bb5493a278f8a7
jakeibeemassa <jac.massa0908@gmail.com>
2015-03-12 13:40:48 -0500

    add insert task to database helper class

--- app/src/main/java/com/infiniteloop/mytasks/data/TaskDataBaseHelper.java
+++ app/src/main/java/com/infiniteloop/mytasks/data/TaskDataBaseHelper.java
@@ -1,8 +1,11 @@
 package com.infiniteloop.mytasks.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.infiniteloop.mytasks.Task;
 import com.infiniteloop.mytasks.data.TaskContract.TaskEntry;
 import com.infiniteloop.mytasks.data.TaskContract.CategoryEntry;
 
@@ -26,22 +29,38 @@ public class TaskDataBaseHelper extends SQLiteOpenHelper {
 
                 TaskEntry.COLUMN_CAT_KEY + " INTEGER NOT NULL, "+
                 TaskEntry.COLUMN_DATE + " INTEGER NOT NULL, "+
-                TaskEntry.COLUMN_TASK_ID + " INTEGER NOT NULL, "+
                 TaskEntry.COLUMN_DURATION + " INTEGER NOT NULL, "+
                 TaskEntry.COLUMN_PRIORITY + " INTEGER NOT NULL, "+
-                TaskEntry.COLUMN_TASK_TITLE + " TEXT NOT NULL, "+
+                TaskEntry.COLUMN_TASK_TITLE + " TEXT NOT NULL";
+//
+//                //set up the location category column as a foreign key to location table.
+//                "FOREIGN KEY (" + TaskEntry.COLUMN_CAT_KEY + ") REFERENCES "+
+//                CategoryEntry.TABLE_NAME + " ("+ CategoryEntry._ID + ");";
 
-                //set up the location category column as a foreign key to location table.
-                "FOREIGN KEY (" + TaskEntry.COLUMN_CAT_KEY + ") REFERENCES "+
-                CategoryEntry.TABLE_NAME + " ("+ CategoryEntry._ID + ");";
-         
+
+
+        final String SQL_CREATE_CATEGORY_TABLE="CREATE TABLE "+ CategoryEntry.TABLE_NAME + " ("+
+                CategoryEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                CategoryEntry.COLUMN_NAME + " TEXT NOT NULL";
+
         db.execSQL(SQL_CREATE_TASK_TABLE);
+        db.execSQL(SQL_CREATE_CATEGORY_TABLE);
 
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //operate schema change and database massage when upgrading
+    }
 
+    public long insertTask(Task task){
+        ContentValues cv = new ContentValues();
+        cv.put(TaskEntry.COLUMN_TASK_TITLE,task.getTitle());
+        cv.put(TaskEntry.COLUMN_DATE,task.getCreationDate());
+        cv.put(TaskEntry.COLUMN_PRIORITY,task.getPriority());
+        cv.put(TaskEntry.COLUMN_DURATION,task.getTotalDurationMinutes());
+        cv.put(TaskEntry.COLUMN_CAT_KEY,task.getCategory());
+        return getWritableDatabase().insert(TaskEntry.TABLE_NAME,null,cv);
     }
 }
