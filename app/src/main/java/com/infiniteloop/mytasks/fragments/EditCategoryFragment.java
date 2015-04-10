c1a3a5c4ec14bc35dc83694387d05357426456f1
jakeibeemassa <jac.massa0908@gmail.com>
2015-04-10 14:13:16 -0500

    Release ready version

--- app/src/main/java/com/infiniteloop/mytasks/fragments/EditCategoryFragment.java
+++ app/src/main/java/com/infiniteloop/mytasks/fragments/EditCategoryFragment.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +13,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.infiniteloop.mytasks.R;
-import com.infiniteloop.mytasks.activities.TaskListActivity;
 import com.infiniteloop.mytasks.data.TaskLab;
 
 /**
