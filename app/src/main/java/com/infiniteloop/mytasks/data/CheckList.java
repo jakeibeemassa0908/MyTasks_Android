88c4daf8d7adcf29a0af7848a62816bb2f2f3e11
jakeibeemassa <jac.massa0908@gmail.com>
2015-04-22 12:12:04 -0500

    Ad CheckList class

---
+++ app/src/main/java/com/infiniteloop/mytasks/data/CheckList.java
@@ -0,0 +1,66 @@
+package com.infiniteloop.mytasks.data;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by theotherside on 22/04/15.
+ */
+public class CheckList {
+
+    private long mId;
+    private long mTaskId;
+    private String mName;
+    private Date mCreatedDate;
+    private Date mEditedDate;
+    private ArrayList<CheckListItems> mChecklistItems;
+
+
+    public long getId() {
+        return mId;
+    }
+
+    public void setId(long mId) {
+        this.mId = mId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+    public Date getCreatedDate() {
+        return mCreatedDate;
+    }
+
+    public void setCreatedDate(Date mCreatedDate) {
+        this.mCreatedDate = mCreatedDate;
+    }
+
+    public Date getEditedDate() {
+        return mEditedDate;
+    }
+
+    public void setEditedDate(Date mEditedDate) {
+        this.mEditedDate = mEditedDate;
+    }
+
+    public ArrayList<CheckListItems> getChecklistItems() {
+        return mChecklistItems;
+    }
+
+    public void setChecklistItems(ArrayList<CheckListItems> mChecklistItems) {
+        this.mChecklistItems = mChecklistItems;
+    }
+
+    public long getTaskId() {
+        return mTaskId;
+    }
+
+    public void setTaskId(long mTaskId) {
+        this.mTaskId = mTaskId;
+    }
+}
---
+++ app/src/main/java/com/infiniteloop/mytasks/data/CheckListItems.java
@@ -0,0 +1,7 @@
+package com.infiniteloop.mytasks.data;
+
+/**
+ * Created by theotherside on 22/04/15.
+ */
+public class CheckListItems {
+}
