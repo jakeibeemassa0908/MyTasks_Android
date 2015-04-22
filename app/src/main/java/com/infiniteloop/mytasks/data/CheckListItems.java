6a74aa9d424aba953f45f1a0a74c04c10914d7b4
jakeibeemassa <jac.massa0908@gmail.com>
2015-04-22 12:12:21 -0500

    Add CheckListItem class

--- app/src/main/java/com/infiniteloop/mytasks/data/CheckListItems.java
+++ app/src/main/java/com/infiniteloop/mytasks/data/CheckListItems.java
@@ -4,4 +4,40 @@ package com.infiniteloop.mytasks.data;
  * Created by theotherside on 22/04/15.
  */
 public class CheckListItems {
+    private long mId;
+    private long mCheckList;
+    private String mItem;
+    private boolean mCompleted;
+
+    public long getId() {
+        return mId;
+    }
+
+    public void setId(long mId) {
+        this.mId = mId;
+    }
+
+    public long getCheckList() {
+        return mCheckList;
+    }
+
+    public void setCheckList(long mCheckList) {
+        this.mCheckList = mCheckList;
+    }
+
+    public String getItem() {
+        return mItem;
+    }
+
+    public void setItem(String mItem) {
+        this.mItem = mItem;
+    }
+
+    public boolean isCompleted() {
+        return mCompleted;
+    }
+
+    public void setCompleted(boolean mCompleted) {
+        this.mCompleted = mCompleted;
+    }
 }
