a17759314fa3392bfb4cf18dd12937c82fdb51b5
jakeibeemassa <jac.massa0908@gmail.com>
2015-04-25 17:52:30 -0500

    Make class parcelable

--- app/src/main/java/com/infiniteloop/mytasks/data/CheckList.java
+++ app/src/main/java/com/infiniteloop/mytasks/data/CheckList.java
@@ -1,12 +1,15 @@
 package com.infiniteloop.mytasks.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 import java.util.Date;
 
 /**
  * Created by theotherside on 22/04/15.
  */
-public class CheckList {
+public class CheckList implements Parcelable {
 
     private long mId;
     private long mTaskId;
@@ -14,6 +17,7 @@ public class CheckList {
     private Date mCreatedDate;
     private Date mEditedDate;
     private ArrayList<CheckListItem> mChecklistItems;
+
 
 
     public long getId() {
@@ -62,5 +66,39 @@ public class CheckList {
 
     public void setTaskId(long mTaskId) {
         this.mTaskId = mTaskId;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mId);
+        dest.writeLong(mTaskId);
+        dest.writeString(mName);
+        dest.writeSerializable(mCreatedDate);
+        dest.writeSerializable(mEditedDate);
+    }
+
+    public static final Parcelable.Creator<CheckList> CREATOR
+            = new Parcelable.Creator<CheckList>() {
+        public CheckList createFromParcel(Parcel in) {
+            return new CheckList(in);
+        }
+
+        public CheckList[] newArray(int size) {
+            return new CheckList[size];
+        }
+    };
+
+    private CheckList(Parcel in){
+        mId = in.readLong();
+        mTaskId = in.readLong();
+        mName=in.readString();
+        mCreatedDate= (Date)in.readSerializable();
+        mEditedDate = (Date)in.readSerializable();
     }
 }
