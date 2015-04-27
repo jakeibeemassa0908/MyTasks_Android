4de25145783ea01670a24c523f2b710a53912abc
jakeibeemassa <jac.massa0908@gmail.com>
2015-04-26 19:28:38 -0500

    Add layout for viewing all notes and checklist

---
+++ app/src/main/java/com/infiniteloop/mytasks/activities/DefaultGridActivity.java
@@ -0,0 +1,7 @@
+package com.infiniteloop.mytasks.activities;
+
+/**
+ * Created by theotherside on 26/04/15.
+ */
+public class DefaultGridActivity {
+}
---
+++ app/src/main/java/com/infiniteloop/mytasks/fragments/DefaultGridFragment.java
@@ -0,0 +1,7 @@
+package com.infiniteloop.mytasks.fragments;
+
+/**
+ * Created by theotherside on 26/04/15.
+ */
+public class DefaultGridFragment {
+}
