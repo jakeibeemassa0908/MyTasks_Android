de2ebb7b77fe0ac0a70af4ccea86430cbc63a3db
jakeibeemassa <jac.massa0908@gmail.com>
2015-04-27 12:17:08 -0500

    Move loader in different file

--- app/src/main/java/com/infiniteloop/mytasks/loaders/CursorLoader.java
+++ app/src/main/java/com/infiniteloop/mytasks/loaders/CursorLoader.java
@@ -9,6 +9,7 @@ import com.infiniteloop.mytasks.data.TaskLab;
  * Created by theotherside on 17/03/15.
  */
 public class CursorLoader {
+
     public static class CategoryListLoader extends SQLiteCursorLoader {
 
         public CategoryListLoader(Context context){
@@ -18,6 +19,50 @@ public class CursorLoader {
         @Override
         protected Cursor loadCursor() {
             return TaskLab.get(getContext()).getCategoryCursor();
+        }
+    }
+
+    public static class NoteCursorLoader extends SQLiteCursorLoader {
+        long mTaskid;
+
+        public NoteCursorLoader(Context context,long taskId){
+            super (context);
+            mTaskid=taskId;
+        }
+
+        @Override
+        protected Cursor loadCursor() {
+            //Query the list of runs
+            return TaskLab.get(getContext()).queryNotes(mTaskid);
+        }
+    }
+
+
+    public static class ChecklistCursorLoader extends SQLiteCursorLoader{
+        long mTaskid;
+
+        public ChecklistCursorLoader(Context context,long taskId){
+            super(context);
+            mTaskid=taskId;
+        }
+
+        @Override
+        protected Cursor loadCursor() {
+            return TaskLab.get(getContext()).queryChecklist(mTaskid);
+        }
+    }
+
+    public static class PhotoCursorLoader extends SQLiteCursorLoader{
+        long mTaskId;
+
+        public PhotoCursorLoader(Context context, long taskId){
+            super(context);
+            mTaskId= taskId;
+        }
+
+        @Override
+        protected Cursor loadCursor() {
+            return TaskLab.get(getContext()).getPhotos(mTaskId);
         }
     }
 
